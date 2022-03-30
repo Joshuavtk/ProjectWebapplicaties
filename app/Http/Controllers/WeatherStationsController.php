c6b52db5aa748014a277c180148581d77f040d56
Joshua <joshuavtk@hotmail.com>
2022-03-30 12:39:51 +0200

    Fix receiving of measurements

--- app/Http/Controllers/WeatherStationsController.php
+++ app/Http/Controllers/WeatherStationsController.php
@@ -22,36 +22,41 @@ class WeatherStationsController extends Controller
     //
     public function receive(Request $request) {
 
+
         if ($request->post('WEATHERDATA') == null) {
-            return;
+            return "Error";
         }
 
         foreach($request->post('WEATHERDATA') as $row) {
             $data = new WeatherData ;
 
-            $frshtt = str_split($row['FRSHTT']);
-
-            $data->station_nr = $row['STN'];
+            $data->station_name = $row['STN'];
             $data->datetime = $row['DATE'] . ' ' . $row['TIME'];
-            $data->temp = $row['TEMP'];
-            $data->dew_point_temp = $row['DEWP'];
-            $data->station_air_pressure = $row['STP'];
-            $data->sea_level_air_pressure = $row['SLP'];
-            $data->visibility = $row['VISIB'];
-            $data->wind_speed = $row['WDSP'];
-            $data->precipitation = $row['PRCP'];
-            $data->snow_depth = $row['SNDP'];
-            $data->frost = $frshtt[0];
-            $data->rain = $frshtt[1];
-            $data->snow = $frshtt[2];
-            $data->hail = $frshtt[3];
-            $data->thunderstorm = $frshtt[4];
-            $data->tornado = $frshtt[5];
-            $data->cloud_cover_percentage = $row['CLDC'];
-            $data->wind_direction = $row['WNDDIR'];
+            $data->temp = $row['TEMP'] == "None" ? null : $row['TEMP'];
+            $data->dew_point_temp = $row['DEWP'] == "None" ? null : $row['DEWP'];
+            $data->station_air_pressure = $row['STP'] == "None" ? null : $row['STP'];
+            $data->sea_level_air_pressure = $row['SLP'] == "None" ? null : $row['SLP'];
+            $data->visibility = $row['VISIB'] == "None" ? null : $row['VISIB'];
+            $data->wind_speed = $row['WDSP'] == "None" ? null : $row['WDSP'];
+            $data->precipitation = $row['PRCP'] == "None" ? null : $row['PRCP'];
+            $data->snow_depth = $row['SNDP'] == "None" ? null : $row['SNDP'];
+
+            if ($row['FRSHTT'] !== "") {
+                $frshtt = str_split($row['FRSHTT']);
+                $data->frost = $frshtt[0];
+                $data->rain = $frshtt[1];
+                $data->snow = $frshtt[2];
+                $data->hail = $frshtt[3];
+                $data->thunderstorm = $frshtt[4];
+                $data->tornado = $frshtt[5];
+            }
+
+            $data->cloud_cover_percentage = $row['CLDC'] == "None" ? null : $row['CLDC'];
+            $data->wind_direction = $row['WNDDIR'] == "None" ? null : $row['WNDDIR'];
 
             $data->save();
-        }
+        }   
+        return "Success";
     }
 
     public function get() {

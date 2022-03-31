28dd5ffb284548010087da4856d84ea161657e5b
Wilke Bouwer <80696402+wilkebouwer@users.noreply.github.com>
2022-03-31 11:55:12 +0200

    Implement correction of binary weather values

--- database/migrations/2022_03_30_023233_add_weather_data_triggers.php
+++ database/migrations/2022_03_30_023233_add_weather_data_triggers.php
@@ -69,6 +69,35 @@ return new class extends Migration {
              IF NEW.wind_direction IS NULL THEN
                 SELECT ROUND(SUM(lastValues.wind_direction) / COUNT(*), 1) INTO @result FROM (SELECT * FROM weather_data WHERE station_name = NEW.station_name ORDER BY id DESC LIMIT 30) AS lastValues;
                 SET NEW.wind_direction = @result;
+                
+             IF NEW.frost IS NULL THEN
+                SELECT ROUND(SUM(lastValues.frost) / COUNT(*), 0) INTO @result FROM (SELECT * FROM measurements WHERE station_id = NEW.station_name ORDER BY id DESC LIMIT 30) AS lastValues;
+                SET NEW.frost = @result 
+             END IF;
+               
+             IF NEW.rain IS NULL THEN
+                SELECT ROUND(SUM(lastValues.rain) / COUNT(*), 0) INTO @result FROM (SELECT * FROM measurements WHERE station_id = NEW.station_name ORDER BY id DESC LIMIT 30) AS lastValues;
+                SET NEW.rain = @result
+             END IF;
+
+             IF NEW.snow IS NULL THEN
+                SELECT ROUND(SUM(lastValues.snow) / COUNT(*), 0) INTO @result FROM (SELECT * FROM measurements WHERE station_id = NEW.station_name ORDER BY id DESC LIMIT 30) AS lastValues;
+                SET NEW.snow = @result
+             END IF;
+
+             IF NEW.hail IS NULL THEN
+                SELECT ROUND(SUM(lastValues.hail) / COUNT(*), 0) INTO @result FROM (SELECT * FROM measurements WHERE station_id = NEW.station_name ORDER BY id DESC LIMIT 30) AS lastValues;
+                SET NEW.hail = @result
+             END IF;
+
+             IF NEW.thunderstorm IS NULL THEN
+                SELECT ROUND(SUM(lastValues.thunderstorm) / COUNT(*), 0) INTO @result FROM (SELECT * FROM measurements WHERE station_id = NEW.station_name ORDER BY id DESC LIMIT 30) AS lastValues;
+                SET NEW.thunderstorm = @result
+             END IF;
+
+             IF NEW.tornado IS NULL THEN
+                SELECT ROUND(SUM(lastValues.tornado) / COUNT(*), 0) INTO @result FROM (SELECT * FROM measurements WHERE station_id = NEW.station_name ORDER BY id DESC LIMIT 30) AS lastValues;
+                SET NEW.tornado = @result 
              END IF;
         END;
 ";

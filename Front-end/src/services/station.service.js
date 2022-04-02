337a18f90e33dbcf648831229451e04ebb6d68d9
Reinier De La Parra <reinierdlp@gmail.com>
2022-04-02 19:41:48 +0200

    Front-end herstelled

---
+++ Front-end/src/services/station.service.js
@@ -0,0 +1,27 @@
+import { axios } from '@/helpers/axios'
+
+export const stationService = {
+    getStationData
+}
+
+function getStationData(stationName) {
+    return axios.get(`/getWeatherData/${stationName}`)
+        .then(handleResponse)
+        .then(station => station)
+}
+
+function handleResponse(response) {
+    if (response.status >= 300) {
+        // if (response.status === 401) {
+        //     // auto logout if 401 response returned from api
+        //     localStorage.removeItem('user')
+        //     delete axios.defaults.headers.common['Authorization']
+        //     // location.reload()
+        // }
+        // TODO: add alert message
+        const error = (response.data && response.data.message) || response.statusText
+        return Promise.reject(error)
+    }
+
+    return response.data
+}

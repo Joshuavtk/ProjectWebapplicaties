7dd189bbc2511788e440ca8a553939c3773d22f9
Reinier De La Parra <reinierdlp@gmail.com>
2022-04-07 21:38:08 +0200

    #fix 46 Gates toe gevoeg in AppServiceProvider En de drie verschillende rollen toegevoegd

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Models\User;
+use Illuminate\Auth\Access\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -15,6 +16,9 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         Gate::define('admin', fn($user) => $user->role == User::USER_ROLE_ADMIN);
+        Gate::define('user',fn($user) => $user->role == User::USER_ROLE_USER);
+        Gate::define('station',fn($user) => $user->role == User::USER_ROLE_USER);
         Gate::define('manage-maintenances', fn($user, $model) => $user->maintenances()->pluck('id')->contains($model->id) || Gate::check('admin'));
+        Gate::define('manage-stations', fn($user, $model) => $user->stations()->pluck('id')->contains($model->id) || Gate::check('admin'));
     }
 }

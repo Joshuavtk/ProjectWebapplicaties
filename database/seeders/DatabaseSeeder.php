e095788ce39bb9927223c1fe9e07f0db1d38e61e
Reinier De La Parra <reinierdlp@gmail.com>
2022-04-18 21:24:02 +0200

    Bugfix migrateion verkeerde table naam

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Subscription;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -17,10 +18,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+
+        DB::table('subscriptions')->insert([
+                ['id' => Str::uuid(), 'name' => 'basic'],
+                ['id' => Str::uuid(), 'name' => 'standard'],
+                ['id' => Str::uuid(), 'name' => 'premium']
+            ]
+        );
+
+        $sobscriptionId = Subscription::first();
+
         DB::table('users')->insert([
-            ['id' => Str::uuid(), 'email' => 'admin@dxmusic.nl', 'password' => Hash::make('BRkuT2awqENoNMpo'), 'role' => \App\Models\User::USER_ROLE_ADMIN],
-            ['id' => Str::uuid(), 'email' => 'partner@dxmusic.nl', 'password' => Hash::make('OnGuEegw36dmUfp1'), 'role' => \App\Models\User::USER_ROLE_USER],
-            ['id' => Str::uuid(), 'email' => 'device@dxmusic.nl', 'password' => Hash::make('i5ETv8ifePTq4mr2'), 'role' => \App\Models\User::USER_ROLE_USER]
+            ['id' => Str::uuid(), 'email' => 'admin@dxmusic.nl', 'password' => Hash::make('BRkuT2awqENoNMpo'), 'subscription_id' => $sobscriptionId, 'role' => \App\Models\User::USER_ROLE_ADMIN],
+            ['id' => Str::uuid(), 'email' => 'partner@dxmusic.nl', 'password' => Hash::make('OnGuEegw36dmUfp1'), 'subscription_id' => $sobscriptionId, 'role' => \App\Models\User::USER_ROLE_USER],
+            ['id' => Str::uuid(), 'email' => 'device@dxmusic.nl', 'password' => Hash::make('i5ETv8ifePTq4mr2'), 'subscription_id' => $sobscriptionId, 'role' => \App\Models\User::USER_ROLE_USER]
         ]);
     }
 }
